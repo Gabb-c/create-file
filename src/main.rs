8f1e5659bcbc5361107b268e5a2c0f0283175b8e
Gabb-c <gdgabrieldacunha71@gmail.com>
2023-03-01 20:24:18 -0300

    feat: create and write to a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::{write, OpenOptions};
+use std::io::{prelude::*, BufReader, Result};
+
+fn main() -> Result<()> {
+    let file = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open("./foo.txt")?;
+
+    write("./foo.txt", "Teste")?;
+
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+
+    buf_reader.read_to_string(&mut contents)?;
+    println!("{contents}");
+
+    Ok(())
 }
